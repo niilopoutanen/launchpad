0bcf95d4dbd212dbbec097f37e8fec302fde56d7
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-06-16 10:55:49 +0300

    Initial working directory

---
+++ LaunchPad/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace LaunchPad
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private LaunchPadWindow launchPadWindow;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Create an instance of HwndSource for the main window
+            var hwndSource = new HwndSource(0, 0, 0, 0, 0, "LaunchPadClass", IntPtr.Zero);
+
+            // Create an instance of CustomHotKey and register the Shift + Tab hotkey
+            var hotKey = new HotKey(hwndSource)
+            {
+                Key = Key.Tab,
+                ModifierKeys = HotKey.Modifiers.Shift
+            };
+
+            hotKey.HotKeyPressed += HotKey_HotKeyPressed;
+            hotKey.Enabled = true;
+        }
+
+        private void HotKey_HotKeyPressed(object sender, HotKeyEventArgs e)
+        {
+            if (launchPadWindow != null && launchPadWindow.IsActive)
+            {
+                launchPadWindow.Close();
+                return;
+            }
+
+            launchPadWindow = new LaunchPadWindow();
+            launchPadWindow.Show();
+        }
+
+    }
+}
---
+++ LaunchPad/HotKey.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.ComponentModel;
+
+namespace System.Windows.Input
+{
+    internal class HotKeyEventArgs : EventArgs
+    {
+        public HotKey Hotkey { get; private set; }
+
+        public HotKeyEventArgs(HotKey hotkey)
+        {
+            Hotkey = hotkey;
+        }
+    }
+
+    internal class HotKey
+    {
+        private const int WM_HOTKEY = 786;
+
+        [DllImport("user32", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        private static extern int RegisterHotKey(IntPtr hwnd, int id, int modifiers, int key);
+
+        [DllImport("user32", CharSet = CharSet.Ansi, SetLastError = true, ExactSpelling = true)]
+        private static extern int UnregisterHotKey(IntPtr hwnd, int id);
+
+        public enum Modifiers : int
+        {
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            Win = 8
+        }
+
+        public Key Key { get; set; }
+        public Modifiers ModifierKeys { get; set; }
+
+        public event EventHandler<HotKeyEventArgs> HotKeyPressed;
+
+        private int id;
+
+        private HwndSourceHook hook;
+        private HwndSource hwndSource;
+
+        private static readonly Random rand = new Random((int)DateTime.Now.Ticks);
+
+        public HotKey(HwndSource hwndSource)
+        {
+            if (hwndSource == null)
+            {
+                throw new ArgumentNullException("hwnd was null");
+            }
+
+            this.hook = new HwndSourceHook(WndProc);
+            this.hwndSource = hwndSource;
+            hwndSource.AddHook(hook);
+
+            id = rand.Next();
+        }
+
+        public void UpdateHwndSource(HwndSource hwndSource)
+        {
+            this.hwndSource = hwndSource;
+        }
+
+        private bool enabled;
+
+        public bool Enabled
+        {
+            get => enabled;
+            set
+            {
+                if (value == enabled) return;
+
+                int handle = (int)hwndSource.Handle;
+                if (handle == 0)
+                {
+                    if (value) throw new ArgumentNullException("Handle");
+                    enabled = value;
+                    return;
+                }
+
+                int result = value ? RegisterHotKey(hwndSource.Handle, id, (int)ModifierKeys, KeyInterop.VirtualKeyFromKey(Key))
+                                   : UnregisterHotKey(hwndSource.Handle, id);
+
+                if (result == 0)
+                {
+                    int error = Marshal.GetLastWin32Error();
+                    if (error != 0) throw new Win32Exception(error);
+                }
+
+                enabled = value;
+            }
+        }
+
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WM_HOTKEY && (int)wParam == id && HotKeyPressed != null)
+            {
+                HotKeyPressed(this, new HotKeyEventArgs(this));
+            }
+
+            return IntPtr.Zero;
+        }
+
+
+        private bool disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                hwndSource.RemoveHook(hook);
+            }
+
+            Enabled = false;
+
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~HotKey()
+        {
+            Dispose();
+        }
+    }
+}
