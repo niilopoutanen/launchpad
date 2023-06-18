75ea0034a161d771b86235f0f85da19cdd039d50
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-06-18 16:51:11 +0300

    Title

--- LaunchPadConfigurator/MainWindow.xaml.cs
+++ LaunchPadConfigurator/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Display.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -32,7 +34,8 @@ namespace LaunchPadConfigurator
             {
                 ChangeSelection(e.SelectedItem as NavigationViewItem);
             };
-            
+            Title = "LaunchPad settings";
+
             ChangeSelection(settingsMenu.MenuItems.FirstOrDefault() as NavigationViewItem);
         }
 
