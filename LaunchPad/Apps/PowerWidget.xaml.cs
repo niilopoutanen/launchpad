bcc14a5e8489aa2a9ea3cfd223e228063a53b51e
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-07-09 18:16:23 +0300

    Code for sleep

--- LaunchPad/Apps/PowerWidget.xaml.cs
+++ LaunchPad/Apps/PowerWidget.xaml.cs
@@ -2,6 +2,7 @@
 using LaunchPadCore.Models;
 using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,7 +16,7 @@ namespace LaunchPad.Apps
         public override bool Pressed { get; set; }
         public override bool Focused { get; set; }
         public override bool WaitForAnim => false;
-        public override bool HasSecondaryAction => false;
+        public override bool HasSecondaryAction => true;
 
         public override FrameworkElement BaseElement => Container;
         public override TextBlock ItemName => VisualName;
@@ -23,6 +24,9 @@ namespace LaunchPad.Apps
 
         public override Widget Widget { get; set; }
         public override int Variation { get; set; }
+
+        [DllImport("PowrProf.dll", SetLastError = true)]
+        public static extern bool SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
 
         public PowerWidget(Widget widget)
         {
@@ -38,23 +42,36 @@ namespace LaunchPad.Apps
                 To = 1.0,
                 Duration = new Duration(TimeSpan.FromSeconds(0.3))
             };
-
-            switch (PowerButton.Visibility)
+            if(ActionConfirmation.Visibility == Visibility.Collapsed)
             {
-                case Visibility.Visible:
-                    PowerButton.Visibility = Visibility.Collapsed;
-                    PowerConfirmation.Visibility = Visibility.Visible;
-                    PowerConfirmation.BeginAnimation(UIElement.OpacityProperty, fadeInAnimation);
-                    break;
+                Variation1.Visibility = Visibility.Collapsed;
+                Variation2.Visibility = Visibility.Collapsed;
+                Variation3.Visibility = Visibility.Collapsed;
 
-                case Visibility.Collapsed:
-                    var shutdownProcess = new ProcessStartInfo("shutdown", "/s /t 0");
-                    shutdownProcess.CreateNoWindow = true;
-                    shutdownProcess.UseShellExecute = false;
-                    Process.Start(shutdownProcess);
-                    break;
+                ActionConfirmation.Visibility = Visibility.Visible;
+                ActionConfirmation.BeginAnimation(UIElement.OpacityProperty, fadeInAnimation);
+                return Task.CompletedTask;
             }
 
+            var powerProcess = new ProcessStartInfo("shutdown", "/s /t 0");
+            switch (Variation)
+            {
+                case 1:
+                    powerProcess = new ProcessStartInfo("shutdown", "/s /t 0");
+                    break;
+
+                case 2:
+                    powerProcess = new ProcessStartInfo("shutdown", "/r /t 0");
+                    break;
+
+                case 3:
+                    SetSuspendState(false, false, false);
+                    break;
+            }
+            powerProcess.CreateNoWindow = true;
+            powerProcess.UseShellExecute = false;
+            Process.Start(powerProcess);
+            ((App)Application.Current).ToggleLaunchpad();
             return Task.CompletedTask;
         }
 
@@ -64,7 +81,7 @@ namespace LaunchPad.Apps
             SolidColorBrush textColor = activeDictionary["LaunchPadTextColor"] as SolidColorBrush;
             if (!Preferences.ThemedWidgets)
             {
-                PowerConfirmation.Foreground = textColor;
+                ActionConfirmation.Foreground = textColor;
             }
         }
     }
