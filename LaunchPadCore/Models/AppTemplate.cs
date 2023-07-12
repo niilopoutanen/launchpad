5f0385b402a419ebb9045bf45c6920392e0484d4
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-07-12 15:46:17 +0300

    Preparing for data loading

---
+++ LaunchPadCore/Models/AppTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchPadCore.Models
+{
+    public class AppTemplate
+    {
+        public string Name { get; set; }
+        public string FamilyId { get; set; }
+        public string AppId { get; set; }
+        public int ID { get; set; }
+
+        public AppTemplate() { }
+
+        public AppTemplate(string name, string familyId, string appId, int id)
+        {
+            Name = name;
+            FamilyId = familyId;
+            AppId = appId;
+            ID = id;
+        }
+    }
+}
---
+++ LaunchPadCore/Utility/DataManager.cs
@@ -0,0 +1,20 @@
+﻿using LaunchPadCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LaunchPadCore.Utility
+{
+    public class DataManager
+    {
+        public List<AppTemplate> GetData()
+        {
+            List<AppTemplate> templateApps = new();
+
+            return templateApps;
+        }
+    }
+}
