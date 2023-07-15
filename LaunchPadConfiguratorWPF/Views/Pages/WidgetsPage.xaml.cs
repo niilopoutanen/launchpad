9610ece8760f28d456167dd348f7d99295884e61
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-07-15 22:16:23 +0300

    Initial widget page

--- LaunchPadConfiguratorWPF/Views/Pages/WidgetsPage.xaml.cs
+++ LaunchPadConfiguratorWPF/Views/Pages/WidgetsPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LaunchPadConfiguratorWPF.Views.Controls;
+using LaunchPadCore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +17,19 @@ using System.Windows.Shapes;
 
 namespace LaunchPadConfiguratorWPF.Views.Pages
 {
-    /// <summary>
-    /// Interaction logic for WidgetsPage.xaml
-    /// </summary>
     public partial class WidgetsPage : Page
     {
+        readonly List<Widget> widgets = new();
+
         public WidgetsPage()
         {
             InitializeComponent();
+            widgets = Widget.LoadWidgets();
+            foreach (Widget widget in widgets)
+            {
+                WidgetGalleryControl listItem = new(widget);
+                WidgetContainer.Children.Add(listItem);
+            }
         }
     }
 }
