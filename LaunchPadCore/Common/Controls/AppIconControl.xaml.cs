99774982cd88e808720a33630fc435ca9c03bdf4
Niilo Poutanen <niilo.poutanen@gmail.com>
2023-07-16 17:24:06 +0300

    Animations + events to icon control

--- LaunchPadCore/Common/Controls/AppIconControl.xaml.cs
+++ LaunchPadCore/Common/Controls/AppIconControl.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -17,34 +18,76 @@ namespace LaunchPadCore.Common.Controls
 {
     public partial class AppIconControl : UserControl
     {
+        private const float SIZE_FOCUS = 1.05f;
+        private const float SIZE_STATIC = 1f;
+        private const float SIZE_PRESSED = 0.9f;
+
         public new string Name 
         {
             get { return VisualName.Text; }
             set { VisualName.Text = value; }
         }
-        public bool NameVisible
+        public TextBlock NameElement
         {
-            get => VisualName.Visibility != Visibility.Collapsed;
-            set => VisualName.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+            get => VisualName;
+            set => VisualName = value;
         }
         public new ImageSource Foreground
         {
             get { return ForegroundImage.Source; }
             set { ForegroundImage.Source = value; }
         }
-        public EventHandler? OnRelease;
-        public EventHandler? OnPress;
+        public bool IsPressed = false;
+        public new bool IsFocused = false;
+        public EventHandler<bool> Pressed;
+        public EventHandler<bool>? Focused;
+        public EventHandler OnClick;
 
         public AppIconControl()
         {
             InitializeComponent();
             Base.MouseLeftButtonDown += (s, e) =>
             {
-                OnPress?.Invoke(this, EventArgs.Empty);
+                IsPressed = true;
+                Pressed?.Invoke(this, IsPressed);
             };
             Base.MouseLeftButtonUp += (s, e) =>
             {
-                OnRelease?.Invoke(this, EventArgs.Empty);
+                IsPressed = false;
+                Pressed?.Invoke(this, IsPressed);
+            };
+            Base.MouseEnter += (s, e) =>
+            {
+                IsFocused = true;
+                Focused?.Invoke(this, IsFocused);
+            };
+            Base.MouseLeave += (s, e) =>
+            {
+                IsFocused = false;
+                Focused?.Invoke(this, IsFocused);
+            };
+
+            Pressed += (s, e) =>
+            {
+                if (e)
+                {
+                    OnPress();
+                }
+                else
+                {
+                    OnRelease();
+                }
+            };
+            Focused += (s, e) =>
+            {
+                if (e)
+                {
+                    OnFocusEnter();
+                }
+                else
+                {
+                    OnFocusLeave();
+                }
             };
         }
         public void SetBackground(ImageSource image)
@@ -55,5 +98,80 @@ namespace LaunchPadCore.Common.Controls
         {
             Container.Background = color;
         }
+
+        public virtual void OnFocusEnter()
+        {
+            ScaleTransform scaleTransform = new(SIZE_STATIC, SIZE_STATIC);
+
+            DoubleAnimation scaleAnimation = new()
+            {
+                To = SIZE_FOCUS,
+                Duration = TimeSpan.FromSeconds(0.1)
+            };
+
+            Container.RenderTransform = scaleTransform;
+
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
+        }
+        public virtual void OnFocusLeave()
+        {
+            ScaleTransform scaleTransform = new(SIZE_FOCUS, SIZE_FOCUS);
+
+            DoubleAnimation scaleAnimation = new()
+            {
+                To = SIZE_STATIC,
+                Duration = TimeSpan.FromSeconds(0.1)
+            };
+
+            Container.RenderTransform = scaleTransform;
+
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
+        }
+
+        public virtual void OnPress()
+        {
+            ScaleTransform scaleTransform = new(SIZE_STATIC, SIZE_STATIC);
+
+            DoubleAnimation scaleAnimation = new()
+            {
+                To = SIZE_PRESSED,
+                Duration = TimeSpan.FromSeconds(0.05)
+            };
+
+
+            Container.RenderTransform = scaleTransform;
+
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
+        }
+        public virtual void OnRelease()
+        {
+            float finalValue = SIZE_STATIC;
+            if (IsFocused)
+            {
+                finalValue = SIZE_FOCUS;
+            }
+            ScaleTransform scaleTransform = new(SIZE_PRESSED, SIZE_PRESSED);
+
+            DoubleAnimationUsingKeyFrames scaleAnimation = new()
+            {
+                Duration = TimeSpan.FromSeconds(0.5)
+            };
+
+            scaleAnimation.KeyFrames.Add(new LinearDoubleKeyFrame(SIZE_PRESSED, TimeSpan.Zero));
+
+            scaleAnimation.KeyFrames.Add(new LinearDoubleKeyFrame(finalValue * 1.05, TimeSpan.FromSeconds(0.1)));
+            scaleAnimation.KeyFrames.Add(new LinearDoubleKeyFrame(finalValue, TimeSpan.FromSeconds(0.25)));
+
+            Container.RenderTransform = scaleTransform;
+
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleXProperty, scaleAnimation);
+            scaleTransform.BeginAnimation(ScaleTransform.ScaleYProperty, scaleAnimation);
+
+            OnClick?.Invoke(this, EventArgs.Empty);
+        }
+
     }
 }
